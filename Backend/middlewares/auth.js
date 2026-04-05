7ca52edaee0f630671dc294c773271da4431756e
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2026-04-05 16:27:37 +0530

    feat: Implement authentication middleware using JWT

---
+++ Backend/middlewares/auth.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+
+
+export const authMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1] || req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ error: "Unauthorized User" });
+    }
+
+    const decode = jwt.verify(token, process.env.SECRET_KEY)
+    console.log('decode : ', decode)
+    req.user = decode;
+    next();
+}
