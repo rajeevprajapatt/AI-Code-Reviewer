683641abc0c26f57e053589e102d13c5fe6567d0
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2026-04-05 16:30:17 +0530

    feat: Initialize Express server with routing and middleware setup

---
+++ Backend/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+import connectDB from './database.js';
+import userRoutes from './routes/user_routes.js';
+import aiRoutes from './routes/ai_routes.js';
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.use('/user', userRoutes)
+app.use('/ai', aiRoutes)
+// console.log(process.env.MONGO_URI);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
