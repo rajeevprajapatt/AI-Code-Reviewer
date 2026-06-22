20c3766396a8706b246fa35cf12258a9fb056d99
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2026-06-22 15:40:20 +0530

    fix: Implement CORS with allowed origins for enhanced security

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,27 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  "https://sleekreview.vercel.app/",
+  "https://sleekreview-git-main-rajeevprajapat43-gmailcoms-projects.vercel.app/"  // Vercel preview URL
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (Postman, mobile apps)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
