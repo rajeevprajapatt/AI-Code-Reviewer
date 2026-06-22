30860e4ac00aec645205d46574378e6f79b29b66
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2026-06-22 15:30:59 +0530

    fix: Improve token extraction logic in authMiddleware for better error handling

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,14 +1,20 @@
 import jwt from 'jsonwebtoken'
 
-
 export const authMiddleware = async (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1] || req.cookies.token;
+
+    const token =
+        req.headers.authorization?.split(' ')[1] ||
+        req.cookies?.token;
 
     if (!token) {
         return res.status(401).json({ error: "Unauthorized User" });
     }
 
-    const decode = jwt.verify(token, process.env.SECRET_KEY)
-    req.user = decode;
-    next();
+    try {
+        const decode = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decode;
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: "Invalid Token" });
+    }
 }
